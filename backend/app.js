45e8f711cd0be20d7f4c506e4313ea056bc1886c
vactiger <vactiger@gmail.com>
2026-02-08 20:35:49 +0800

    更新：Part 1 初階任務

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/healthcheck', (req, res) => {
   res.status(200)
-  res.send('OK')
+  res.status(200).json({
+    status: 'success',
+    message: 'OK'
+  })
 })
 app.use('/api/credit-package', creditPackageRouter)
 app.use('/api/coaches/skill', skillRouter)
